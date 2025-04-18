fbbe9b324febdc36a41ab3b7e5bcd82fad81c841
MrTom9te <gabriel_santarem@outlook.com>
2025-04-18 12:57:09 -0400

    atividade01

---
+++ atividade01/UmPrograma.java
@@ -0,0 +1,23 @@
+// Atividade 1
+// • Observe o programa abaixo
+//
+// ```java
+//  package umprograma;
+// class UmPrograma {
+// public static void main(String[] args) {
+// System.out.println(“Olá, mundo!”);
+// }
+// }
+// ```
+//
+// 1. Altere o programa para imprimir uma
+// mensagem diferente.
+// 2. Altere o programa para imprimir DUAS
+// mensagens diferentes.
+class UmPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Minha primeira mensagem diferente!");
+        System.out.println("Minha segunda mensagem diferente!");
+    }
+}
